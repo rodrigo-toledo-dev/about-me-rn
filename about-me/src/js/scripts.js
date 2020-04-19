0dbc2f807893e35cf978d5c7f1bb3d6cd341894b
Rodrigo Toledo <rodrigo.toledo@superare.co>
2020-04-19 09:42:05 -0300

    add new components fixed the layout to use only pure html

--- about-me/src/js/scripts.js
+++ about-me/src/js/scripts.js
@@ -1,18 +1,21 @@
+
+import jQuery from 'jquery';
+
 /*!
     * Start Bootstrap - Freelancer v6.0.1 (https://startbootstrap.com/themes/freelancer)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
     */
-    (function($) {
+const scripts = function(){
     "use strict"; // Start of use strict
   
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    jQuery('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (window.location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && window.location.hostname == this.hostname) {
+        var target = jQuery(this.hash);
+        target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
-          $('html, body').animate({
+          jQuery('html, body').animate({
             scrollTop: (target.offset().top - 71)
           }, 1000, "easeInOutExpo");
           return false;
@@ -21,49 +24,51 @@
     });
   
     // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
+    jQuery(document).scroll(function() {
+      var scrollDistance = jQuery(this).scrollTop();
       if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
+        jQuery('.scroll-to-top').fadeIn();
       } else {
-        $('.scroll-to-top').fadeOut();
+        jQuery('.scroll-to-top').fadeOut();
       }
     });
   
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
+    jQuery('.js-scroll-trigger').click(function() {
+      jQuery('.navbar-collapse').collapse('hide');
     });
   
     // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
+    jQuery('body').scrollspy({
       target: '#mainNav',
       offset: 80
     });
   
     // Collapse Navbar
     var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
+      if (jQuery("#mainNav").offset().top > 100) {
+        jQuery("#mainNav").addClass("navbar-shrink");
       } else {
-        $("#mainNav").removeClass("navbar-shrink");
+        jQuery("#mainNav").removeClass("navbar-shrink");
       }
     };
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    jQuery(window).scroll(navbarCollapse);
   
     // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    jQuery(function() {
+      jQuery("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        jQuery(this).toggleClass("floating-label-form-group-with-value", !!jQuery(e.target).val());
       }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
+        jQuery(this).addClass("floating-label-form-group-with-focus");
       }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
+        jQuery(this).removeClass("floating-label-form-group-with-focus");
       });
     });
   
-  })(jQuery); // End of use strict
+  }
   
+
+  export default scripts;
